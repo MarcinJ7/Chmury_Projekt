a410ba8eb68e2ea64ec33afb9dd5dfd3008b37e0
malwinq <malwina.kubas@gmail.com>
2020-04-13 22:48:30 +0200

    Update test

--- NN_Model/test_model.py
+++ NN_Model/test_model.py
@@ -5,6 +5,11 @@ Test the trained model
 """
 
 from tensorflow.keras.models import model_from_json
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+path_dir = r'D:/chmury/imdb/preprocessed/'            # path of preprocessed images
 
 # Wczytanie modelu
 json_file = open(path_dir + 'model_cnn.json', 'r')
@@ -15,26 +20,24 @@ model.load_weights(path_dir + 'model_cnn.h5')
 print('Loaded model from disk')
 
 # Test
-image = cv2.imread(path_dir + 'dataset/test/07/14853007_1984-05-07_2007.jpg')
-image = cv2.resize(image, (tar_size, tar_size))
-b,g,r = cv2.split(image)        # opencv ma format BGR, więc trzeba przekształcić na RGB
-image2 = cv2.merge([r,g,b])
+image = cv2.imread(path_dir + '460682.jpg')
+b, g, r = cv2.split(image)        # opencv ma format BGR, więc trzeba przekształcić na RGB
+image = cv2.merge([r / 255, g / 255, b / 255])
 plt.imshow(image, cmap='gray')
 plt.axis("off")
 plt.show()
 
-X = np.expand_dims(image, axis=[0, 3])
+X = np.expand_dims(image, axis=[0])
 y_pred = model.predict(X)[0][0]
 print('Wiek: ', y_pred)
 
-image = cv2.imread(path_dir + 'dataset/test/07/440507_1969-12-10_2014.jpg')
-image = cv2.resize(image, (tar_size, tar_size))
+image = cv2.imread(path_dir + '459951.jpg')
 b,g,r = cv2.split(image)        # opencv ma format BGR, więc trzeba przekształcić na RGB
-image2 = cv2.merge([r,g,b])
-plt.imshow(image, cmap='gray')
+image = cv2.merge([r / 255, g / 255, b / 255])
+plt.imshow(image)
 plt.axis("off")
 plt.show()
 
-X = np.expand_dims(image, axis=[0, 3])
+X = np.expand_dims(image, axis=[0])
 y_pred = model.predict(X)[0][0]
 print('Wiek: ', y_pred)
