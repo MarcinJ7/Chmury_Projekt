9526cf730c919906bb04e473ae3404429b804192
malwinq <malwina.kubas@gmail.com>
2020-04-13 12:40:49 +0200

    Fix bugs

--- NN_Model/data_preparation.py
+++ NN_Model/data_preparation.py
@@ -1,5 +1,91 @@
 """
-Prepare the csv file for data preprocessing
+Preparation of data: face detection, cropping images and making a csv file
 
 @author: Malwina
 """
+import os
+from scipy.io import loadmat
+import cv2
+import csv
+
+
+# params
+path_input = r'D:/chmury/imdb/imdb_crop/imdb_crop/'      # path of the dataset
+path_output = r'D:/chmury/imdb/preprocessed/'            # path of preprocessed images
+# path of the haar cascade classifier
+path_classifier = r'C:/Users/cp/Documents/GitHub/Chmury_Projekt/Face_Detection/haarcascade_frontalface_default.xml'
+target_size = (299, 299)
+
+face_cascade = cv2.CascadeClassifier(path_classifier)
+
+if not os.path.exists(path_output):
+    os.makedirs(path_output)
+
+path_mat = path_input + r'/imdb.mat'
+mat_data = loadmat(path_mat)
+mat_data = mat_data['imdb']
+
+mat_paths = mat_data[0][0][2][0]
+mat_gender = mat_data[0][0][3][0]
+
+# tables of csv files to train and test network
+output_train = []
+output_train.append(['path', 'age', 'gender'])
+output_test = []
+output_test.append(['path', 'age', 'gender'])
+
+i = 0
+j = -1
+for path in mat_paths:
+    path = str(path[0])
+    path_new = path_input + path
+    image = cv2.imread(path_new)
+    j += 1
+    if type(image) == 'NoneType':
+        continue
+    path_save = path_output + str(i) + '.jpg'
+    row = []
+
+    # age calculation
+    d1 = path.split('_')[2]
+    d1 = d1.split('-')[0]
+    d2 = path.split('_')[-1][:-4]
+    age = int(d2) - int(d1)
+
+    # face detection (@Marcin)
+    faces = face_cascade.detectMultiScale(image, 1.3, 5)
+    for (x, y, w, h), k in zip(faces, list(range(len(faces)))):
+        crop_img = image[y:y + h, x:x + w]
+        crop_img = cv2.resize(crop_img, target_size)
+        cv2.imwrite(path_save, crop_img)
+        break
+
+    row.append(path_save)
+    row.append(age)
+    row.append(mat_gender[j])
+
+    # 20% of data (1/5) will be saved to test file
+    if i % 5 == 0:
+        output_test.append(row)
+    else:
+        output_train.append(row)
+    i += 1
+
+print('Preprocessed', i, 'images')
+print('Number of training photos:', len(output_train))
+print('Number of testing photos:', len(output_test))
+
+# save csv files
+path_file = path_output + 'dataset_train.csv'
+with open(path_file, 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(output_train)
+
+path_file = path_output + 'dataset_test.csv'
+with open(path_file, 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(output_test)
+
+print('Files saved')
+
+
--- NN_Model/model.py
+++ NN_Model/model.py
@@ -9,51 +9,41 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras import optimizers
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import model_from_json
 import matplotlib.pyplot as plt
 import pandas as pd
 
-tar_size = 512
+# params
+tar_size = 299
 shape = (tar_size, tar_size, 3)
+path_dir = r'D:/chmury/imdb/preprocessed/'            # path of preprocessed images
+
+# definicja modelu sieci
 
 model = tensorflow.keras.Sequential()  # rodzaj modelu sieci
-
 # 1. Warstwa konwolucyjna, rozmiar okna: 3x3, głębia jądra: 32
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=shape))
 model.add(layers.MaxPooling2D((2, 2)))
-
 # 2. Warstwa konwolucyjna, rozmiar okna: 3x3, głębia jądra: 64
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
-
 # 3. Warstwa konwolucyjna, rozmiar okna: 3x3, głębia jądra: 128
 model.add(layers.Conv2D(128, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
-
+# 4. Warstwa konwolucyjna, rozmiar okna: 3x3, głębia jądra: 256
 model.add(layers.Conv2D(256, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
-
-# 4. Warstwa spłaszczająca dane (z 2D na 1D)
+# 5. Warstwa spłaszczająca dane (z 2D na 1D)
 model.add(layers.Flatten())
-
-# 5. Warstwa odrzucająca 20% danych
+# 6. Warstwa odrzucająca 20% danych
 model.add(layers.Dropout(0.2))
-
-# 6. Warstwa gęsta, 256 neuronów
+# 7. Warstwa gęsta, 256 neuronów
 model.add(layers.Dense(256, activation='relu'))
-
-# 7. Warstwa wyjściowa - 1 neuron, bez funkcji aktywacji bo wyjściem ma być wartość wyjściowa (wiek)
+# 8. Warstwa wyjściowa - 1 neuron, bez funkcji aktywacji bo wyjściem ma być wartość wyjściowa (wiek)
 model.add(layers.Dense(1))
-
 # Funkcja printująca podsumowanie modelu sieci
 model.summary()
 
-# Optymalizator - Adam (prawie zawsze daje najlepsze wyniki)
 optim = optimizers.Adam()
-
-# Kompilacja modelu, loss - funkcja straty, metrics - metryka
-# Funkcja straty: mse, czyli błąd średniokwadratowy, bo wyjściem jest liczba a nie klasa
-# Metryka: mae, czyli średni błąd bezwględny, tak jak wyżej
 model.compile(loss='mse', optimizer=optim, metrics=['mae'])
 
 callbacks = [
@@ -75,22 +65,19 @@ callbacks = [
     ),
 ]
 
+# Zapisanie modelu do pliku json (model) i h5 (wagi)
+model_json = model.to_json()
+with open(path_dir + 'model_cnn.json', 'w') as json_file:
+    json_file.write(model_json)
+print('Saved model to disk')
+
 # Wczytanie danych do pandas df
-path_dir = r'/content/drive/My Drive/colab_files/imdb/'
-df = pd.read_csv(path_dir + 'train.csv', sep=',', header=0, names=['filepath', 'age'])
-df_val = pd.read_csv(path_dir + 'test.csv', sep=',', header=0, names=['filepath', 'age'])
+df = pd.read_csv(path_dir + 'dataset_train.csv', sep=',', header=0, names=['path', 'age', 'gender'])
+df_val = pd.read_csv(path_dir + 'dataset_test.csv', sep=',', header=0, names=['path', 'age', 'gender'])
 
 # Przetworzenie zdjęć z augumentacją, czyli "dorabianiem" nowych zdjęc
 # dane treningowe
-train_datagen = ImageDataGenerator(
-    rescale=1. / 255,
-    rotation_range=10,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True)
-
+train_datagen = ImageDataGenerator(rescale=1. / 255)
 # dane testowe
 test_datagen = ImageDataGenerator(rescale=1. / 255)
 
@@ -98,26 +85,26 @@ test_datagen = ImageDataGenerator(rescale=1. / 255)
 train_generator = train_datagen.flow_from_dataframe(
     dataframe=df,
     directory='',
-    x_col='filepath',
+    x_col='path',
     y_col='age',
     target_size=(tar_size, tar_size),
-    batch_size=128,
-    class_mode='raw', )
+    batch_size=64,
+    class_mode='raw')
 
 validation_generator = test_datagen.flow_from_dataframe(
     dataframe=df_val,
     directory='',
-    x_col='filepath',
+    x_col='path',
     y_col='age',
     target_size=(tar_size, tar_size),
-    batch_size=128,
-    class_mode='raw', )
+    batch_size=64,
+    class_mode='raw')
 
 # Wytrenowanie modelu
 history = model.fit(
     train_generator,
-    steps_per_epoch=20,
-    epochs=30,
+    steps_per_epoch=100,
+    epochs=50,
     validation_data=validation_generator,
     callbacks=callbacks)
 
@@ -136,11 +123,3 @@ plt.legend()
 plt.grid(True)
 
 plt.show()
-
-# Zapisanie modelu do pliku json (model) i h5 (wagi)
-model_json = model.to_json()
-with open(path_dir + 'model_cnn.json', 'w') as json_file:
-    json_file.write(model_json)
-
-# model.save_weights(path_dir + 'model_cnn.h5')
-print('Saved model to disk')
