ead95d4680207e87536e265d728dbfd23334a48c
MarcinJ7 <49407119+MarcinJ7@users.noreply.github.com>
2020-05-06 10:55:41 +0200

    Create getApiJson.py
    Create script which send a request to Face Cognitive Services endpoint on Azure (responce - data in JSON format)

---
+++ Cognitive Services/getApiJson.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 14:13:42 2020
+
+@author: Marcin
+"""
+
+import requests
+import json
+import cv2 
+import numpy as np
+from urllib.request import urlopen
+
+subscription_key = 'here paste your key'
+
+face_api_url = 'https://...ENDPOINT.../face/v1.0/detect'
+
+# Example photo url
+image_url = 'https://cms-tc.pbskids.org/parents/expert-tips-and-advice/helping-young-children-learn-to-make-friends-hero.jpg?mtime=20181008025027'
+
+req = urlopen(image_url)
+arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+img = cv2.imdecode(arr, -1)
+
+headers = {'Ocp-Apim-Subscription-Key': subscription_key}
+
+params = {
+    'returnFaceId': 'true',
+    'returnFaceLandmarks': 'false',
+    'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise',
+}
+
+response = requests.post(face_api_url, params=params, headers=headers, json={"url": image_url})
+
+x  = response.json()
+
+# Wiek:
+for i in x:
+    print("Wykryto osobe, wiek: " + str(i["faceAttributes"]["age"]))
+    face_rect = i["faceRectangle"]
+    img = cv2.rectangle(img,(face_rect['left'],face_rect['top']),(face_rect['left']+face_rect['width'],face_rect['top']+face_rect['height']),(122,122,255),2)
+    img = cv2.putText(img, ("Wiek: " + str(i["faceAttributes"]["age"])) , (face_rect['left'], face_rect['top']-20) , cv2.FONT_HERSHEY_SIMPLEX, 1, (100,100,255) , 2 , cv2.LINE_AA) 
+
+cv2.imshow("Our Picture", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
