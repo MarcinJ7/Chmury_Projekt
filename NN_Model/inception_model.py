772c57b6c66fc05f18083935b9e0abf42a9fa0d5
malwinq <malwina.kubas@gmail.com>
2020-05-23 10:32:16 +0200

    Add paths

--- NN_Model/inception_model.py
+++ NN_Model/inception_model.py
@@ -18,7 +18,8 @@ import pandas as pd
 tar_size = 299
 
 # wczytanie modelu
-base_model = InceptionV3(weights = 'imagenet', include_top = False, input_shape=(tar_size, tar_size, 3))
+base_model = InceptionV3(weights='imagenet', include_top=False, input_shape=(tar_size, tar_size, 3))
+path_dir = r'D:/chmury/imdb/preprocessed/'            # path of preprocessed images
 
 # dorobienie warstw wyjściowych
 x = base_model.output
@@ -27,7 +28,7 @@ x = Dense(512, activation='relu')(x)
 predictions = Dense(1, activation=None)(x)
 
 # ostateczny model
-model = Model(inputs = base_model.input, outputs = predictions)
+model = Model(inputs=base_model.input, outputs=predictions)
 
 # zamrożenie warstw
 for layer in base_model.layers:
@@ -39,22 +40,22 @@ optim = optimizers.Adam()
 model.compile(optim, loss='mse', metrics=['mae'])
 
 callbacks = [
-				keras.callbacks.ModelCheckpoint(  		# zapisywanie najlepszego modelu
-						filepath=path_dir + 'model_inc.h5',
-						monitor='val_loss',
-						save_best_only=True,
-				),
+    keras.callbacks.ModelCheckpoint(  # zapisywanie najlepszego modelu
+        filepath=path_dir + 'model_inc.h5',
+        monitor='val_loss',
+        save_best_only=True,
+    ),
 
-				keras.callbacks.EarlyStopping(      # zatrzymywanie uczenia po braku poprawy metryki
-						monitor='mae',
-						patience=4,
-    		),
+    keras.callbacks.EarlyStopping(  # zatrzymywanie uczenia po braku poprawy metryki
+        monitor='mae',
+        patience=4,
+    ),
 
-				keras.callbacks.ReduceLROnPlateau(      # poprawianie lr po utknięciu w minimum
-						monitor='val_loss',
-						factor=0.1,
-						patience=4,
-  		   ),
+    keras.callbacks.ReduceLROnPlateau(  # poprawianie lr po utknięciu w minimum
+        monitor='val_loss',
+        factor=0.1,
+        patience=4,
+    ),
 ]
 
 model_json = model.to_json()
@@ -67,39 +68,39 @@ df_val = pd.read_csv(path_dir + 'test.csv', sep=',', header=0, names=['filepath'
 
 # Przetworzenie zdjęć
 # dane treningowe
-train_datagen = ImageDataGenerator(rescale=1./255)
+train_datagen = ImageDataGenerator(rescale=1. / 255)
 
 # dane testowe
-test_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1. / 255)
 
 # Generatory do uczenia sieci
 train_generator = train_datagen.flow_from_dataframe(
-												dataframe=df,
-												directory='',
-												x_col='filepath',
-												y_col='age',
-												target_size=(tar_size, tar_size),
-												batch_size=128,
-												class_mode='raw',
-                        seed=10,	)
+    dataframe=df,
+    directory='',
+    x_col='filepath',
+    y_col='age',
+    target_size=(tar_size, tar_size),
+    batch_size=128,
+    class_mode='raw',
+    seed=10, )
 
 val_generator = test_datagen.flow_from_dataframe(
-												dataframe=df_val,
-												directory='',
-												x_col='filepath',
-												y_col='age',
-												target_size=(tar_size, tar_size),
-												batch_size=128,
-												class_mode='raw',
-                        seed=10,   )
+    dataframe=df_val,
+    directory='',
+    x_col='filepath',
+    y_col='age',
+    target_size=(tar_size, tar_size),
+    batch_size=128,
+    class_mode='raw',
+    seed=10, )
 
 history = model.fit_generator(train_generator,
-                      steps_per_epoch = 175,
-                      validation_data = val_generator,
-                      validation_steps = 44,
-                      epochs = 10,
-                      verbose = 2,
-                      callbacks = callbacks)
+                              steps_per_epoch=175,
+                              validation_data=val_generator,
+                              validation_steps=44,
+                              epochs=10,
+                              verbose=2,
+                              callbacks=callbacks)
 
 # Wykres
 loss = history.history['loss']
